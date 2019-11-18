1f509e63d020d99e65225dfee63a660ffce4d376
Jor Sanders <jor.sanders@capgemini.com>
2019-11-18 15:37:36 +0100

    fix routing for room add and 404

--- hotel.webapp/src/app/app-routing.module.ts
+++ hotel.webapp/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { RoomsComponent } from './rooms/rooms.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { RoomAddComponent } from './room-add/room-add.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'rooms', component: RoomsComponent},
-  {path: 'room/add', component: PageNotFoundComponent},
+  {path: 'room/add', component: RoomAddComponent},
+  {path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
