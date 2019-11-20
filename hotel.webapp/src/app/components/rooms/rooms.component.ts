829071f05e8492e99cabd110333a40e5a7ef7371
Jor Sanders <jor.sanders@capgemini.com>
2019-11-20 16:37:03 +0100

    refactor rooms component ts

--- hotel.webapp/src/app/components/rooms/rooms.component.ts
+++ hotel.webapp/src/app/components/rooms/rooms.component.ts
@@ -19,13 +19,13 @@ import { fromEvent } from 'rxjs';
 
 export class RoomsComponent implements OnInit {
   displayedColumns = ['id', 'name', 'capacity'];
-  exampleDatabase: HotelApiService | null;
+  hotelApiService: HotelApiService | null;
   dataSource: RoomsDataSource | null;
   index: number;
   id: number;
 
   constructor(public httpClient: HttpClient,
-    public dialog: MatDialog,
+    public matDialog: MatDialog,
     public dataService: HotelApiService) { }
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
@@ -41,53 +41,44 @@ export class RoomsComponent implements OnInit {
   }
 
   addNew(issue: Room) {
-    const dialogRef = this.dialog.open(AddDialogComponent, {
+    const dialogRef = this.matDialog.open(AddDialogComponent, {
       data: { issue: issue }
     });
 
     dialogRef.afterClosed().subscribe(result => {
       if (result === 1) {
-        // After dialog is closed we're doing frontend updates
-        // For add we're just pushing a new row inside DataService
-        this.exampleDatabase.dataChange.value.push(this.dataService.getDialogData());
+        this.hotelApiService.dataChange.value.push(this.dataService.getDialogData());
         this.refreshTable();
       }
     });
   }
 
-  startEdit(i: number, id: number, title: string, state: string, url: string, created_at: string, updated_at: string) {
+  startEdit(id: number, name: string, capacity: number) {
     this.id = id;
-    // index row is used just for debugging proposes and can be removed
-    this.index = i;
-    console.log(this.index);
-    const dialogRef = this.dialog.open(EditDialogComponent, {
-      data: { id: id, title: title, state: state, url: url, created_at: created_at, updated_at: updated_at }
+    const dialogRef = this.matDialog.open(EditDialogComponent, {
+      data: { id: id, name: name, capacity: capacity }
     });
 
     dialogRef.afterClosed().subscribe(result => {
       if (result === 1) {
-        // When using an edit things are little different, firstly we find record inside DataService by id
-        const foundIndex = this.exampleDatabase.dataChange.value.findIndex(x => x.id === this.id);
-        // Then you update that record using data from dialogData (values you enetered)
-        this.exampleDatabase.dataChange.value[foundIndex] = this.dataService.getDialogData();
-        // And lastly refresh table
+        const foundIndex = this.hotelApiService.dataChange.value.findIndex(x => x.id === this.id);
+        this.hotelApiService.dataChange.value[foundIndex] = this.dataService.getDialogData();
         this.refreshTable();
       }
     });
   }
 
-  deleteItem(i: number, id: number, title: string, state: string, url: string) {
+  deleteItem(i: number, id: number, name: string, capacity: number) {
     this.index = i;
     this.id = id;
-    const dialogRef = this.dialog.open(DeleteDialogComponent, {
-      data: { id: id, title: title, state: state, url: url }
+    const dialogRef = this.matDialog.open(DeleteDialogComponent, {
+      data: { id: id, name: name, capacity: capacity }
     });
 
     dialogRef.afterClosed().subscribe(result => {
       if (result === 1) {
-        const foundIndex = this.exampleDatabase.dataChange.value.findIndex(x => x.id === this.id);
-        // for delete we use splice in order to remove single object from DataService
-        this.exampleDatabase.dataChange.value.splice(foundIndex, 1);
+        const foundIndex = this.hotelApiService.dataChange.value.findIndex(x => x.id === this.id);
+        this.hotelApiService.dataChange.value.splice(foundIndex, 1);
         this.refreshTable();
       }
     });
@@ -96,34 +87,13 @@ export class RoomsComponent implements OnInit {
 
   private refreshTable() {
     // Refreshing table using paginator
-    // Thanks yeager-j for tips
-    // https://github.com/marinantonio/angular-mat-table-crud/issues/12
     this.paginator._changePageSize(this.paginator.pageSize);
   }
 
-
-  /*   // If you don't need a filter or a pagination this can be simplified, you just use code from else block
-    // OLD METHOD:
-    // if there's a paginator active we're using it for refresh
-    if (this.dataSource._paginator.hasNextPage()) {
-      this.dataSource._paginator.nextPage();
-      this.dataSource._paginator.previousPage();
-      // in case we're on last page this if will tick
-    } else if (this.dataSource._paginator.hasPreviousPage()) {
-      this.dataSource._paginator.previousPage();
-      this.dataSource._paginator.nextPage();
-      // in all other cases including active filter we do it like this
-    } else {
-      this.dataSource.filter = '';
-      this.dataSource.filter = this.filter.nativeElement.value;
-    }*/
-
   public loadData() {
-    this.exampleDatabase = new HotelApiService(this.httpClient);
-    this.dataSource = new RoomsDataSource(this.exampleDatabase, this.paginator, this.sort);
+    this.hotelApiService = new HotelApiService(this.httpClient);
+    this.dataSource = new RoomsDataSource(this.hotelApiService, this.paginator, this.sort);
     fromEvent(this.filter.nativeElement, 'keyup')
-      // .debounceTime(150)
-      // .distinctUntilChanged()
       .subscribe(() => {
         if (!this.dataSource) {
           return;
