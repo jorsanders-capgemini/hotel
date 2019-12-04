22aef4ef17713ecf6d71273ed48fa458452388ac
Jor Sanders <jor.sanders@capgemini.com>
2019-12-04 14:30:14 +0100

    WIP readd edit screen

--- hotel.webapp/src/app/components/rooms/room-edit/room-edit.component.ts
+++ hotel.webapp/src/app/components/rooms/room-edit/room-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RoomsService } from 'src/app/services/rooms.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Room } from 'src/app/models/room';
+import { roomForm } from '../roomForm';
 
 @Component({
   selector: 'app-room-edit',
@@ -9,12 +10,25 @@ import { Room } from 'src/app/models/room';
   styleUrls: ['./room-edit.component.scss']
 })
 export class RoomEditComponent implements OnInit {
-  constructor(protected roomsService: RoomsService, protected router: Router) {}
+  constructor(private router: Router, private route: ActivatedRoute, private roomsService: RoomsService) {}
 
-  ngOnInit() {}
+  private room: Room;
+
+  ngOnInit() {
+    this.room = new Room();
+
+    this.routeSub = this.route.params.subscribe(params => {
+      this.roomsService.getRoom(params.id).subscribe(result => {
+        this.room = result;
+        roomForm.get('name').setValue(this.room.name);
+        roomForm.get('capacity').setValue(this.room.capacity);
+      });
+    });
+  }
   onSubmit(roomData: any) {
-    const room = new Room().deserialize(roomData);
-    this.roomsService.createRoom(room).subscribe(() => {
+    this.room.name = roomData.name;
+    this.room.capacity = roomData.capacity;
+    this.roomsService.updateRoom(this.room).subscribe(() => {
       this.router.navigate(['/kamers']);
     });
   }
