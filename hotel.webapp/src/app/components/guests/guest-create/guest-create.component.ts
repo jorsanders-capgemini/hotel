8511b7fb69003aba232508d308e009c8c9258cd2
Jor Sanders <jor.sanders@capgemini.com>
2019-12-09 14:51:22 +0100

    Added guest components to angular

---
+++ hotel.webapp/src/app/components/guests/guest-create/guest-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-create',
+  templateUrl: './guest-create.component.html',
+  styleUrls: ['./guest-create.component.scss']
+})
+export class GuestCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
