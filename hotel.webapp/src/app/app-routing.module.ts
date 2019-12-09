20d410252030d59c09f81c5829c93ff1f4818f60
Jor Sanders <jor.sanders@capgemini.com>
2019-12-09 16:37:17 +0100

    Added routing/navigation for guest

--- hotel.webapp/src/app/app-routing.module.ts
+++ hotel.webapp/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { RoomListComponent } from './components/rooms/room-list/room-list.component';
 import { RoomCreateComponent } from './components/rooms/room-create/room-create.component';
 import { RoomEditComponent } from './components/rooms/room-edit/room-edit.component';
+import { GuestEditComponent } from './components/guests/guest-edit/guest-edit.component';
+import { GuestListComponent } from './components/guests/guest-list/guest-list.component';
+import { GuestCreateComponent } from './components/guests/guest-create/guest-create.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'rooms', pathMatch: 'full' },
   { path: 'rooms', component: RoomListComponent },
   { path: 'rooms/new', component: RoomCreateComponent },
   { path: 'rooms/:id', component: RoomEditComponent },
+  { path: 'guests', component: GuestListComponent },
+  { path: 'guests/new', component: GuestCreateComponent },
+  { path: 'guests/:id', component: GuestEditComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
