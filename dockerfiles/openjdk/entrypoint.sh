e1b1307e2c73c05cee5bc84f8531c70880f3f456
Jor Sanders <jor.sanders@capgemini.com>
2020-01-03 13:06:38 +0100

    fix java run command

--- dockerfiles/openjdk/entrypoint.sh
+++ dockerfiles/openjdk/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn package
-java -cp target/hotel-0.0.1.jar com.jor.hotel.HotelApplication
+mvn -Dmaven.test.skip=true package
+java -jar target/hotel-0.0.1.jar com.jor.hotel.HotelApplication
