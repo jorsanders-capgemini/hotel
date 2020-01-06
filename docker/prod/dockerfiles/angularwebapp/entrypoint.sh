aea2faac675a7726184756d910418d5a0486f10a
Jor Sanders <jor.sanders@capgemini.com>
2020-01-06 16:35:46 +0100

    prod image for springapi still need to create different in app configs

--- docker/prod/dockerfiles/angularwebapp/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-npm install
-ng serve --host 0.0.0.0 --poll 2000
--- docker/prod/dockerfiles/springapi/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mvn -Dmaven.test.skip=true package
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=*:5005,server=y,suspend=n \
-  -jar target/hotel-0.0.1.jar com.jor.hotel.HotelApplication
