e74004bf12d70ff3f3143baa6e0a1fc0a44e8fe7
Jor Sanders <jor.sanders@capgemini.com>
2020-01-07 16:33:03 +0100

    Working state but needs heavy refactor

--- docker/prod/build/dockerfiles/angularwebapp/entrypoint.sh
+++ docker/prod/build/dockerfiles/angularwebapp/entrypoint.sh
@@ -1,11 +1,11 @@
 # Clean up, commands may fail if directory is empty
 rm -rf /app/dist/hotel/* || true
 
-# rm -rf doesn't work well with wildcards and nested non empty directories.
-# And we cant delete the volume mounted by docker. So first clean out all files
+# rm -rf doesn't work for some reason on nested not empty directories
+# So first clean out all files
 # this will give warnings in the console but cant fixed easily
 find /app/compiled/* -delete || true
-cd /app/compiled rm -rf /app/compiled/* || true
+cd /app/compiled && rm -rf * || true
 
 npm install
 ng build --prod
--- docker/prod/build/dockerfiles/springapi/entrypoint.sh
+++ docker/prod/build/dockerfiles/springapi/entrypoint.sh
@@ -1,8 +1,8 @@
-# rm -rf doesn't work well with wildcards and nested non empty directories.
-# And we cant delete the volume mounted by docker. So first clean out all files
+# rm -rf doesn't work for some reason on nested not empty directories
+# So first clean out all files
 # this will give warnings in the console but cant fixed easily
 find /app/compiled/* -delete || true
-rm -rf /app/compiled/* || true
+cd /app/compiled && rm -rf * || true
 
 mvn -Dmaven.test.skip=true package
 
