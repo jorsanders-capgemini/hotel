91be5a9f4dd13b5dd650bcd8e5645a617ed11f72
Jor Sanders <jor.sanders@capgemini.com>
2020-01-07 15:56:40 +0100

    build is almost working. just cannot clear the compiled directories

--- docker/prod/build/dockerfiles/springapi/entrypoint.sh
+++ docker/prod/build/dockerfiles/springapi/entrypoint.sh
@@ -1,12 +1,9 @@
-# Clean up, commands may fail if directory is empty
-rm -rf /app/dist/hotel/* || true
-
 # rm -rf doesn't work well with wildcards and nested non empty directories.
 # And we cant delete the volume mounted by docker. So first clean out all files
 # this will give warnings in the console but cant fixed easily
 find /app/compiled/* -delete || true
-cd /app/compiled rm -rf /app/compiled/* || true
-#
-#npm install
-#ng build --prod
-#cp -r /app/dist/hotel/* /app/compiled
+rm -rf /app/compiled/* || true
+
+mvn -Dmaven.test.skip=true package
+
+cp -r /app/target/* /app/compiled
