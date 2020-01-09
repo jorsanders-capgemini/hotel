3217778a2d93edcc45b6bdb9bd7121cb4deb404f
Jor Sanders <jor.sanders@capgemini.com>
2020-01-09 16:54:03 +0100

    fix typo

--- docker/hotel-dev/dockerfiles/springapi/entrypoint.sh
+++ docker/hotel-dev/dockerfiles/springapi/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mvn -Dmaven.test.skip=true --no-snaphot-updates package
+mvn -Dmaven.test.skip=true --no-snapshot-updates package
 java -Xdebug -Xrunjdwp:transport=dt_socket,address=*:5005,server=y,suspend=n \
   -jar target/hotel-0.0.1.jar com.jor.hotel.HotelApplication
